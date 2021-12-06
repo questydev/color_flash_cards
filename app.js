2387076fbbc12db0b92b2f4cf1662749b519453c
questydev <rctiernan@gmail.com>
2021-12-06 00:22:50 -0500

    Animal Flash Cards Update

--- app.js
+++ app.js
@@ -1,19 +1,48 @@
-const cardColor = document.querySelector(".card");
+const cardColor = document.querySelector(".colorCard");
+const cardAnimal = document.querySelector(".animalCard");
+
 let colorArray = [
   "red",
   "blue",
   "yellow",
-  "white",
   "black",
   "purple",
   "green",
   "orange",
   "gray",
   "pink",
+  "lightblue",
+  "limegreen",
 ];
+
+let animalArray = [];
+
+//Get Animal Pics from API
+async function getData() {
+  const res = await fetch(
+    "https://api.unsplash.com/search/photos?page=1&query=animals&client_id=wV366wG17ZFzGewQZOAlk7KuMBpSFNL9zQroLafIRyc"
+  );
+
+  const { results } = await res.json();
+
+  console.log(results);
+
+  results.forEach((animal) => {
+    const picList = animal.urls.regular;
+    animalArray.push(picList);
+  });
+
+  colorChange();
+  animalChange();
+}
+
+getData();
 
 let colorRemoved = "";
 let prevColorRemoved = "";
+
+let animalRemoved = "";
+let prevAnimalRemoved = "";
 
 function colorChange() {
   //Randomize the Array Selection
@@ -33,4 +62,32 @@ function colorChange() {
   //console.log(colorArray, colorRemoved);
 }
 
-setInterval(colorChange, 5000);
+function animalChange() {
+  //Randomize the Array Selection
+  const random = Math.floor(Math.random() * animalArray.length);
+  //Apply new Random selection to the variable
+  animalRemoved = animalArray[random];
+  //Change Background Animal
+  cardAnimal.style.backgroundImage = `url('${animalRemoved}')`;
+  //Add previous Animal back into Array after background is applied
+  if (prevAnimalRemoved !== "") {
+    animalArray.push(prevAnimalRemoved);
+  }
+  //Remove the current Animal selected
+  animalArray.splice(animalArray.indexOf(animalRemoved), 1);
+  //Apply current removed Animal to variable that will be added back to the list next round
+  prevAnimalRemoved = animalRemoved;
+  console.log(animalArray, animalRemoved);
+}
+
+//setInterval(colorChange, 5000);
+//setInterval(animalChange, 5000);
+
+// event = keyup or keydown
+document.addEventListener("keypress", (event) => {
+  if (event.code === "Space") {
+    //console.log("Space pressed");
+    colorChange();
+    animalChange();
+  }
+});
